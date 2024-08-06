fc632be0ccc9ada05cdbd614c18ec40e220c583c
JulioC-25 <julio.cesar.hernandez.ramirez.21@gmail.com>
2024-08-05 20:05:36 -0400

    Create botlogic.py

---
+++ botlogic.py
@@ -0,0 +1,12 @@
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+from bot_logic import gen_pass
+print( gen_pass(20))
